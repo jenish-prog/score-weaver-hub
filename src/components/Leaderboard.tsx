5a91ba4ae26844c59c424b7545f3055a123418c7
Jenish <jenish112005@gmail.com>
2025-08-12 09:21:28 +0000

    mobile response

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -216,6 +216,13 @@ export default function Leaderboard() {
           <div className="mb-16">
             <h2 className="text-3xl font-bold text-center mb-8 text-primary">Champions</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-4xl mx-auto">
+
+              {/* Gold - 1st place */}
+              {topThree[0] && (
+                <div className="md:order-2">
+                  <PodiumCard member={topThree[0]} position={1} />
+                </div>
+              )}
               {/* Silver - 2nd place */}
               {topThree[1] && (
                 <div className="md:order-1">
@@ -223,12 +230,7 @@ export default function Leaderboard() {
                 </div>
               )}
               
-              {/* Gold - 1st place */}
-              {topThree[0] && (
-                <div className="md:order-2">
-                  <PodiumCard member={topThree[0]} position={1} />
-                </div>
-              )}
+              
               
               {/* Bronze - 3rd place */}
               {topThree[2] && (
